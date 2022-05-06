6bced47c681e4515826a753f749193de981c4607
陳昱瑋 <103249300+ChenYuWei-Jason@users.noreply.github.com>
2022-05-06 14:33:32 +0800

    Add files via upload

---
+++ LinqLabs (Solution)/LinqLabs/3. FrmLINQ架構介紹_InsideLINQ.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Starter
+{
+    public partial class FrmLINQ架構介紹_InsideLINQ : Form
+    {
+        public FrmLINQ架構介紹_InsideLINQ()
+        {
+            InitializeComponent();
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            System.Collections.ArrayList arrlist = new System.Collections.ArrayList();
+            arrlist.Add(3);
+            arrlist.Add(4);
+            arrlist.Add(1);
+
+            var q = from n in arrlist.Cast<int>()
+                    where n > 2
+                    select new { N= n };
+
+           this.dataGridView1.DataSource =  q.ToList();
+
+           
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = null;
+
+            this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
+
+            var q = (from p in this.nwDataSet1.Products
+                     orderby p.UnitsInStock descending
+                     select p).Take(5);
+
+           this.dataGridView1.DataSource =  q.ToList();
+
+           // this.nwDataSet1.Products.Distinct
+        }
+    }
+}
